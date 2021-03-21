3b0b5d2c5ecb641448f52ffc4ff3cc83dbf061d4
louddonut <roanpaulus@outlook.com>
2021-03-21 15:18:28 +0100

    Added a mouseover color randomizer

--- script.js
+++ script.js
@@ -1,18 +1,60 @@
-function addPixel(gridSize) {
+function addPixel(gridSize, gridRoot) {
 	const sketchContainer = document.querySelector(".sketch-container");
 	const pixel = document.createElement("div");
 	
-	pixel.style.border = "0.1em solid white";
+	pixel.style.border = "1px solid black";
 	pixel.style.boxSizing = "border-box";
-	let size = (1 / gridRoot) * 100;
+	let size = (1 / gridRoot) * 100; //Calculate evenly divided space
 	size = size + "%";
 	pixel.style.height = size;
 	pixel.style.width = size;
+	pixel.style.borderRadius = "0%"; //Put on 50% for circle edition
 	sketchContainer.appendChild(pixel);
 }
 
-let gridRoot = 20;
-let gridSize = gridRoot * gridRoot;
-for (i = 0; i < gridSize; i++) {
-	addPixel(gridSize);
+function generateGrid(gridRoot) {
+	let gridSize = gridRoot * gridRoot; //Total ammount of pixels (Area)
+	for (i = 0; i < gridSize; i++) {
+		addPixel(gridSize, gridRoot);
+	}
 }
+
+function colorPixels(pixelLocation) {
+	let pixel = document.querySelector(pixelLocation);
+	let randomHex = Math.floor(Math.random() * 1000000);
+	console.log(pixel);
+}
+
+function randomColor() {
+	let color = "#";
+	for (let i = 0; i < 6; i++) {
+		const random = Math.random();
+		const bit = (random * 16) | 0;
+		color += (bit).toString(16);
+	}
+	return color;
+}
+
+function colorPixels() {
+const allPixels = document.querySelectorAll(".sketch-container > div");
+
+allPixels.forEach((pixel) => {
+	pixel.addEventListener("mouseover", (e) => {
+		let randomHex = randomColor();
+		e.target.style.backgroundColor = randomHex;
+	});
+});
+}
+
+function etchASketch (size) {
+	generateGrid(size);
+	colorPixels();
+}
+
+etchASketch(16);
+
+//
+
+
+
+
